17ff8990b3acb709bcdcb171123b0af676259185
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-03 21:00:56 +0300

    IEntityRepository olusturuldu. Ve imza niteligindeki metotlari genele hitafen olusturuldu.

---
+++ DateAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        //Bütün ürünleri çek getir.
+        List<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter); 
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        //Şartlı listeleme
+        List<T> List(Expression<Func<T, bool>> filter);
+
+
+    }
+
+}
