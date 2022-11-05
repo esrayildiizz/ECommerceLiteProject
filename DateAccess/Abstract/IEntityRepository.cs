40c55bc9d3886ecbd1c44bcaf2452abc84ab7608
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-05 23:44:11 +0300

    T tipindeki class in IEntity olmasi gerektigini vurguladik.

--- DateAccess/Abstract/IEntityRepository.cs
+++ DateAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
@@ -7,7 +8,7 @@ namespace DataAccess.Abstract
 {
     //T tipi için kısıtlama getirdik yani herkes istediğini yazamasın.
     //generic constaint :kısıtlama
-    public interface IEntityRepository<T> where T:class
+    public interface IEntityRepository<T> where T:class,IEntity
     {
         //Bütün ürünleri çek getir.
         //Şartlı listeleme
