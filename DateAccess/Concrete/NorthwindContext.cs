5e3d22f1a173cbbbd0134217ab998edab7b7da0a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-09 16:49:43 +0300

    Projemin hangi veri tabani ile iliskili oldugunu belirtim.

--- DateAccess/Concrete/NorthwindContext.cs
+++ DateAccess/Concrete/NorthwindContext.cs
@@ -9,8 +9,15 @@ namespace DataAccess.Concrete
     //Context : Db tabloları ile proje class larını bağlar.
     public class NorthwindContext:DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Product> Products { get; set; }
+        //projem hangi veri tabanı ile ilişkili 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        //public DbSet<Category> Categories { get; set; }
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Product> Products { get; set; }
     }
 }
