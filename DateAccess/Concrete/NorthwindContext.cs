5715bf00242f5b83f66854cec26a036f27bd2f33
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-09 16:47:19 +0300

    Db tabloları ile proje class larını baglamak icin NorthwindContext sinifi olusturuldu.

---
+++ DateAccess/Concrete/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    //Context : Db tabloları ile proje class larını bağlar.
+    public class NorthwindContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
