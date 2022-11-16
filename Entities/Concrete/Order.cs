de2fb66c40e84019648d5000ed3a2546d55a662a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-16 17:29:54 +0300

    Order duzenlendi.

--- Entities/Concrete/Order.cs
+++ Entities/Concrete/Order.cs
@@ -6,5 +6,14 @@ namespace Entities.Concrete
 {
     public class Order
     {
+        public int OrderId { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string ShipCity { get; set; }
     }
 }
