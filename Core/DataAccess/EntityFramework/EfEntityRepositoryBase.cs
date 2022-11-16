15b968235a8b7c1ce1ed0874d3569de415e242bb
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-16 15:37:25 +0300

    T tiplerinin ne olacagi sarti yazildi.

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.DataAccess.EntityFramework
 {
     public class EfEntityRepositoryBase<TEntity,TContext>
+        where TEntity:class,IEntity,new()
+        where TContext:DbContext
+
     {
     }
 }
