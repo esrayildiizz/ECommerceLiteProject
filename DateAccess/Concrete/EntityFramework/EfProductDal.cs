4736a60c3dbab4eab7a44d046a6167db0e2de134
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-16 17:09:49 +0300

    EfEntityRepositoryBase dahil edildi.

--- DateAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DateAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,65 +10,8 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-        public void Add(Product entity)
-        {
-            //Bu yapı c# a özel bir yapı:
-            //NorthwindContext bellekte işi bitince atılsın.
-            using (NorthwindContext context =new NorthwindContext())
-            {
-                //git veri kaynağından benim gönderdiğim product dan bir tane nesneye eşleştir.
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-           
-        }
-
-        public void Delete(Product entity)
-        {
-            //Bu yapı c# a özel bir yapı:
-            //NorthwindContext bellekte işi bitince atılsın.
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                //git veri kaynağından benim gönderdiğim product dan bir tane nesneye eşleştir.
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter) //tek datagetiricek unutma.
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter); //filter a göre bir tane değer döndür.
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                 
-                return filter == null //filtre = null mı
-                    ? context.Set<Product>().ToList()  //değil ise Product a yerleş
-                    : context.Set<Product>().Where(filter).ToList(); //null ise
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            //Bu yapı c# a özel bir yapı:
-            //NorthwindContext bellekte işi bitince atılsın.
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                //git veri kaynağından benim gönderdiğim product dan bir tane nesneye eşleştir.
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
+        
     }
 }
