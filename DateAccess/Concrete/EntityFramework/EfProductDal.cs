2ea187ada43dbeec75475c02e6961ac1ba27ea81
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-17 14:23:18 +0300

    Duzenleme yapildi.

--- DateAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DateAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,25 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context=new NorthwindContext())
+            {
+                //ürünler ile kategorileri id lerine göre joın yap.
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.ProductId equals c.CategoryId
+                             select new ProductDetailDto //productdetailDto daki colonlara göre join işlemini yap demek.
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
