7c03002cd5b20e05bd452c59ef7edd239e49c06b
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-17 10:24:58 +0300

    Yeni metot eklendi.

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -5,8 +5,11 @@ using System.Text;
 
 namespace Business.Abstract
 {
+    //Category ile ilgili dış dünyaya neyi servis etmek istiyorsam o operasyonları yazıyorum.
     public interface ICategoryService
     {
         List<Category> GetAll();
+
+        Category GetById(int categoryId); //tek bir categori getireceği için List yazmadık.
     }
 }
