1a3b9785e26f12a3de85f71560c61b59980bc29a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-17 11:10:28 +0300

    Test islemi gerceklestirildi.

--- ConsoleeUI/Program.cs
+++ ConsoleeUI/Program.cs
@@ -9,13 +9,28 @@ namespace ConsoleeUI
     {
         static void Main(string[] args)
         {
+            //CategoryTest();
+            //ProductTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetByUnıtPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
-            
         }
     }
 }
