2eebf7447e0743523830853daa318301494b0f52
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-17 09:30:51 +0300

    IEntity den inheritance alindi.

--- Entities/Concrete/Order.cs
+++ Entities/Concrete/Order.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Order
+    public class Order:IEntity
     {
         public int OrderId { get; set; }
 
