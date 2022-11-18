85256207bd50fd452097885e7e82e4e0f988b483
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-18 12:03:40 +0300

    Yeni service eklendi.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -8,9 +8,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
+        List<Product> GetAll(); //Liste şeklinde ürün döndürür.
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnıtPrice(decimal min, decimal max);
         List<ProductDetailDto> GetProductDetails();
+        Product GetById(int productId); //tek başına bir ürün döndürür.
+        void Add(Product product); //void olduğu için birşey döndürmüyor.
     }
 }
