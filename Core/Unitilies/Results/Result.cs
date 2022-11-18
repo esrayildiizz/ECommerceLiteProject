73449f4b4dd20439cb8bfd54b4cc2e71b8efcbcc
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-18 13:43:34 +0300

    Iplemente islemi yapildi ve constructor metot eklendi.

---
+++ Core/Unitilies/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Unitilies.Results
+{
+    // Results dosyası içinceki somut class olarak düşün.
+    public class Result : IResult
+    {
+        //constructor metotun kendi içinde başka yapılarla çalışmasına mükemmel bir örnek.
+        //ilk result çalıştığında ben ikincisininde çalışmasını istiyorsam : koyup istediğimi yani ikinciyi(tek parametreliyi) yazabilirim.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
