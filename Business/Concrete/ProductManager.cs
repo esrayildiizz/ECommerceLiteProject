0f047deafefaba8b86f3a00bdf95ad4fc5ecc36a
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-18 12:04:15 +0300

    Iplemente edildi ve metot ici dolduruldu.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,6 +19,13 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public void Add(Product product)
+        {
+            //Business kodlarını buraya yazarız.
+            //örneğin kurallar vs varsa onlara bakıp sonra Add işlemi gerçekleşsin.
+            _productDal.Add(product);
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -27,6 +34,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetByUnıtPrice(decimal min, decimal max)
