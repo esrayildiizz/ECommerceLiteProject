afbcae064a36e669ada6d51a25f173d6a015da0f
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-19 15:15:49 +0300

    Kullancagim surekli mesajlar icin sabit static messages class ı olusturdum.

---
+++ Business/Contants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Contants
+{
+    //static tanımlama sebebim Messages sınıfında ram de ayrılan bir kısım olsun ve ben buna new lemeden ulaşabileyim.
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz..";
+    }
+}
