4da1a2c102d67f5c75f657e76744e41f9169a3e2
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-19 20:59:37 +0300

    Yeni results eklendi.

---
+++ Core/Unitilies/Results/ErrorDataResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Unitilies.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, string message) : base(data, false, message)
+        {
+
+        }
+
+        //mesaj işine girmek istemeyebilir.
+        public ErrorDataResult(T data) : base(data, false)
+        {
+
+        }
+
+
+        //datayı default haliyle döndürmek isteyebilir.
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+
+        }
+
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
+    }
+}
---
+++ Core/Unitilies/Results/SuccessDataResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Unitilies.Results
+{
+    public class SuccessDataResult<T>:DataResult<T>
+    {
+        public SuccessDataResult(T data,string message):base(data,true,message)
+        {
+
+        }
+
+        //mesaj işine girmek istemeyebilir.
+        public SuccessDataResult(T data):base(data,true)
+        {
+
+        }
+
+
+        //datayı default haliyle döndürmek isteyebilir.
+        public SuccessDataResult(string message):base(default,true,message)
+        {
+
+        }
+
+        public SuccessDataResult():base(default,true)
+        {
+
+        }
+    }
+}
