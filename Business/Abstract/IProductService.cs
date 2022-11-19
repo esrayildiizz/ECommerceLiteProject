811703804e130ac3701dfa308c71e59f3b9f4214
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-19 16:44:54 +0300

    Tum service leri IDataResult a uyarlandi.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,11 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll(); //Liste şeklinde ürün döndürür.
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnıtPrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
-        Product GetById(int productId); //tek başına bir ürün döndürür.
+        IDataResult<List<Product>>GetAll(); //Liste şeklinde ürün döndürür.
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnıtPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>>GetProductDetails();
+        IDataResult<Product> GetById(int productId); //tek başına bir ürün döndürür.
         IResult Add(Product product); //void olduğu için birşey döndürmüyor. biz void yerine bundan sonra IResult yazdık.
     }
 }
