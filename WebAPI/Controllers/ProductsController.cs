c837693b01a6af467f00a7269913ef5f0ad2c751
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-21 19:48:48 +0300

    ProductsController olusturuldu.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  //attrıbute:bir class ile ilgili bilgi verme 
+    public class ProductsController : ControllerBase
+    {
+    }
+}
