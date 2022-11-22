4cc942ac349c7c8a2cb32eba12c1ead33e96f53c
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-22 21:54:01 +0300

    Duzenleme yapildi.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,16 @@ namespace WebAPI.Controllers
     [ApiController]  //attrıbute:bir class ile ilgili bilgi verme 
     public class ProductsController : ControllerBase
     {
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId=1, ProductName="Elma"},
+                new Product{ProductId=2, ProductName="Armut"},
+            };
+        }
+             
     }
 }
