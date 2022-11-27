88236c5cf654ee6a3ada64a1dfcb174f3724dd32
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-27 14:49:54 +0300

    Startup icine yazdiklarimizi buraya yaziyoruz

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        //uygulama ayağa kalktığında çalışıcak yer.
+        protected override void Load(ContainerBuilder builder)
+        {
+            //product service isterse ona product manager instance ı ver.
+            builder.RegisterType<ProductManager>().As<IProductService>();
+        }
+    }
+}
